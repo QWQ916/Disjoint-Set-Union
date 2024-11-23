6ec4c4325625fe941bf2a6b9642f0ad3c11c7d26
QWQ916 <Shirok2005@icloud.com>
2024-11-23 18:35:21 +0300

    Add files via upload

---
+++ DSU.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Referat1
+{
+    public class DSU
+    {
+        int size; int[] p; int rank_a, rank_b;
+        public DSU(int size, int[] p, int rank_a, int rank_b)
+        {
+            this.size = size; this.p = new int[size]; this.p = p;
+            this.rank_a = rank_a; this.rank_b = rank_b;
+        }
+        public int Find(int x)
+        {
+            if (p[x - 1] == x) return x;
+            else return p[x - 1] = Find(p[x - 1]);
+        }
+        public void Unite(int x, int y)
+        {
+            int x0 = Find(x);
+            int y0 = Find(y); 
+            if (x0 != y0)
+            {
+                if (rank_a > rank_b)
+                {
+                    p[x0 - 1] = y0;
+                }
+                else if (rank_a < rank_b)
+                {
+                    p[y0 - 1] = x0;
+                }
+                else
+                {
+                    p[x0 - 1] = y0;
+                    rank_a++;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Referat1;
+
+class Program
+{
+    private static void Main()
+    {
+        //    элементы графа    1, 2, 3,  4, 5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19
+        int[] p = new int[19] { 4, 4, 13, 4, 12, 14, 13, 10, 3,  4,  2, 12,  8,  5,  0,  0,  0,  0, 5};
+        int[] p0 = p;
+        DSU m1 = new DSU(19, p0, 6, 4);
+        int e1 = 3;
+        int e2 = 6;
+        Console.WriteLine($"Множества к которым принадлежат X = {e1} и Y = {e2}");
+        Console.WriteLine($"X = {e1} в множестве с корнем {m1.Find(e1)}");
+        Console.WriteLine($"Y = {e2} в множестве с корнем {m1.Find(e2)}\n");
+        Console.WriteLine("Теперь объединим эти множества!"); m1.Unite(e2, e1);
+        Console.WriteLine($"X = {e1} в множестве с корнем {m1.Find(e1)}");
+        Console.WriteLine($"Y = {e2} в множестве с корнем {m1.Find(e2)}\n");
+        Console.ReadKey();
+    }
+}
